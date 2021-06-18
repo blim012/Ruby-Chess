cc6d3798730e7c4f657f813394f8b530af14fb69
Brandon Lim <brandonlimchi@gmail.com>
2021-06-18 02:58:31 -0700

    rename subject name for searchable tests

--- spec/searchable_spec.rb
+++ spec/searchable_spec.rb
@@ -2,13 +2,12 @@ require './lib/searchable.rb'
 
 describe Searchable do
   let(:dummy_chessboard) { Class.new {include Searchable} }
+  subject(:search_board) { dummy_chessboard.new }
 
   describe '#find_squares' do
-    subject(:bb_to_square) { dummy_chessboard.new }
-
     it 'returns an array of squares corresponding to set bits in a bitboard' do
       bitboard = 0x2000040000800800
-      squares = bb_to_square.find_squares(bitboard)
+      squares = search_board.find_squares(bitboard)
 
       expect(squares).to eq([52, 40, 21, 2])
     end
